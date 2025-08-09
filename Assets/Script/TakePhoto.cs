a57187a4c2644b04a6d0ff360826a688a1a20115
Jqinnn627 <jiaqin627@gmail.com>
2025-08-09 16:42:41 +0800

    TakePhoto(Private) + Sound

---
+++ Assets/Script/TakePhoto.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakePhoto : MonoBehaviour
+{
+    public Button captureButton; // assign in Inspector
+    public string fileNamePrefix = "ARPhoto_";
+
+    private void Start()
+    {
+        if (captureButton != null)
+            captureButton.onClick.AddListener(PhotoTakingHandler);
+    }
+
+    public void PhotoTakingHandler()
+    {
+        StartCoroutine(CaptureScreenshot());
+    }
+
+    private IEnumerator CaptureScreenshot()
+    {
+        yield return new WaitForEndOfFrame(); // wait until frame is rendered
+
+        // Create a texture the size of the screen
+        Texture2D screenImage = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        // Read screen contents into the texture
+        screenImage.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenImage.Apply();
+
+        // Encode texture to PNG
+        byte[] imageBytes = screenImage.EncodeToPNG();
+
+        // Save to persistent data path
+        string timestamp = System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string fileName = fileNamePrefix + timestamp + ".png";
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+        File.WriteAllBytes(filePath, imageBytes);
+
+        Debug.Log("Photo saved to: " + filePath);
+
+    }
+}
