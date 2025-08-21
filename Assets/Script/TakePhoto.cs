29e9efd9cbe0a98705bdce450171d7239da49613
Jqinnn627 <jiaqin627@gmail.com>
2025-08-21 10:32:16 +0800

    cannot take photo

--- Assets/Script/TakePhoto.cs
+++ Assets/Script/TakePhoto.cs
@@ -1,57 +1,28 @@
 using System.Collections;
-using System.IO;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TakePhoto : MonoBehaviour
 {
-    public Button captureButton; // assign in Inspector
-    public string fileNamePrefix = "ARPhoto_";
-
-    private void Start()
+    public void TakeScreenshotButton()
     {
-        if (captureButton != null)
-            captureButton.onClick.AddListener(PhotoTakingHandler);
+        StartCoroutine(TakeScreenshotAndSave());
     }
 
-    public void PhotoTakingHandler()
+    private IEnumerator TakeScreenshotAndSave()
     {
-        StartCoroutine(CaptureScreenshot());
-    }
+        yield return new WaitForEndOfFrame(); // Wait until the frame is rendered
 
-    private IEnumerator CaptureScreenshot()
-    {
-        yield return new WaitForEndOfFrame(); // wait until frame is rendered
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
 
-        // Create a texture the size of the screen
-        Texture2D screenImage = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        // Save the screenshot to the Gallery/Photos
+        NativeGallery.SaveImageToGallery(ss, "GalleryTest", "Image.png", (success, path) =>
+        {
+            Debug.Log("Media save result: " + success + " Path: " + path);
+        });
 
-        // Read screen contents into the texture
-        screenImage.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        screenImage.Apply();
-
-        // Encode texture to PNG
-        byte[] imageBytes = screenImage.EncodeToPNG();
-
-        // Save to persistent data path
-        string timestamp = System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
-        string fileName = fileNamePrefix + timestamp + ".png";
-        //string folderPath = "/storage/emulated/0/Pictures/Screenshots";
-        // Change to public path
-        //string pubfilName = folderPath + fileName;
-        string filePath = Path.Combine(Application.persistentDataPath, fileName);
-
-        File.WriteAllBytes(filePath, imageBytes);
-
-        Debug.Log("Photo saved to: " + filePath);
-
-#if UNITY_ANDROID
-
-        AndroidJavaClass mediaScan = new AndroidJavaClass("android.media.MediaScannerConnection");
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject context = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-        mediaScan.CallStatic("scanFile", context, new string[] { filePath }, null, null);
-#endif
-
+        // Free memory
+        Destroy(ss);
     }
 }
